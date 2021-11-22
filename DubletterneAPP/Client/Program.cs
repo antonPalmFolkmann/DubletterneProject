00ee025041ce4e16b2b71ef81413e61fa3f25e7d
Anton Folkmann <anpf@itu.dk>
2021-11-22 13:36:23 +0100

    Added Blazor Project

---
+++ DubletterneAPP/Client/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using DubletterneAPP.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("DubletterneAPP.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("DubletterneAPP.ServerAPI"));
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://api://94eabc4d-57ab-4d90-b5d8-92dad5c49f2e/API.Access");
+    options.ProviderOptions.LoginMode = "redirect";
+});
+
+await builder.Build().RunAsync();
