f0ee5d950f39ece3020fc603043c787d5d581b77
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2021-11-26 14:37:46 +0100

    Launch files

--- DubletterneAPP/Client/Program.cs
+++ DubletterneAPP/Client/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().Cre
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://api://94eabc4d-57ab-4d90-b5d8-92dad5c49f2e/API.Access");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("api://94eabc4d-57ab-4d90-b5d8-92dad5c49f2e/API.Access");
     options.ProviderOptions.LoginMode = "redirect";
 });
 
