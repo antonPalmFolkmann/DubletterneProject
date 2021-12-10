f6272dc474b7f03b0e7ecc936db499c0fb013dcc
Slas1 <89983981+Slas1@users.noreply.github.com>
2021-12-10 10:03:44 +0100

    Search API ikke implmenteret (#33)

--- DubletterneAPP/Infrastructure.Tests/search/SeachLogicTests.cs
+++ DubletterneAPP/Infrastructure.Tests/search/SeachLogicTests.cs
@@ -1,21 +1,73 @@
 using Xunit;
-using Infrastructure;
+using Infrastructure.Search;
 
 namespace Infrastructure.Tests.Search;
 
     public class SearchLogicTests : IDisposable{
+    
+    [Theory]
+    [InlineData("a", SearchParameter.Resource)]
+    public void SearchMain(string input, SearchParameter parameter){
+        //Arrange
 
+        //Act
 
+        //Assert
+    }
 
+    [Theory]
+    [InlineData(new string[]{"A","B","C"}, new int[]{100,300,200}, new int[]{1,2,0})]
+    [InlineData(new string[]{"A","B","C","D"}, new int[]{100,200,300,400}, new int[]{3,2,1,0})]
+    [InlineData(new string[]{"A","B","C","D","E"}, new int[]{300,100,4000,400,10}, new int[]{2,3,0,1,4})]
+    public void sortMatchesHighestValueFirst_sorts_highest_first(string[] keys, int[] values, int[] expectedIndexSort){
+        //Arrange
+        var dictionaryPreSort = new Dictionary<string, int>();
+        for (int i = 0; i < keys.Count(); i++){
+            dictionaryPreSort.Add(keys[i], values[i]);
+        }
 
+        var expected = new List<KeyValuePair<string, int>>(){};
+        foreach (int i in expectedIndexSort){
+            expected.Add(new KeyValuePair<string, int>(keys[i], values[i]));
+        }
 
+        //Act
+        var actually = SearchLogic.sortMatchesHighestValueFirst(dictionaryPreSort);
+        
 
+        //Assert
+        Assert.Equal(expected, actually);
+    }
 
+    [Theory]
+    [InlineData()]
+    public void scoreMatch_scores_word_based_onLength(){
+        //Arrange
 
+        //Act
 
+        //Assert
 
+    }
 
+    [Theory]
+    [InlineData()]
+    public void scoreMatch_scores_word_based_onX(){
+        //Arrange
 
+        //Act
+
+        //Assert
+
+    }
+
+    /*
+    private static void searchUser(){
+
+    private static void searchCatagory(){
+
+    private static Dictionary<string, int> searchResourse(string[] terms){
+    */
 
         private bool disposedValue;
 
