51ca5fe852ef7bda3c4f9182ec05cdcca14ccaf2
Nicklas Jørgensen <ncjo@itu.dk>
2021-12-12 18:34:27 +0100

    trying to get access token from the client

--- DubletterneAPP/Infrastructure/AadAuthenticationDbConnectionInterceptor.cs
+++ DubletterneAPP/Infrastructure/AadAuthenticationDbConnectionInterceptor.cs
@@ -10,7 +10,7 @@ namespace Infrastructure
         private const string TENANT_ID = "33a5d5c9-1cff-47b3-a604-c8d39aa4931f";
         private const string CLIENT_ID = "94eabc4d-57ab-4d90-b5d8-92dad5c49f2e"; // Azure_SQL
         private const string CLIENT_SECRET = "x~-7Q~th26rtfoBxGJx3aZF0IZAWedtFuXhGM";
-        private const string REDIRECT_URI = "http://localhost:7072";
+        private const string REDIRECT_URI = "http://localhost:7073";
         private const string CONNECTION_STRING = "Server=tcp:dubletterne.database.windows.net,1433;Initial Catalog=Project;Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False";
         private static readonly string[] scopes = new string[] { "https://database.windows.net/.default" };
         public override async ValueTask<InterceptionResult> ConnectionOpeningAsync(
@@ -29,6 +29,49 @@ namespace Infrastructure
             
             return await base.ConnectionOpeningAsync(connection,eventData,result,cancellationToken);
         }
+
+        public static async Task<string> GetAccessToken_UserInteractive()
+        {
+        
+            IPublicClientApplication app = PublicClientApplicationBuilder
+                .Create(CLIENT_ID)
+                .WithAuthority(AzureCloudInstance.AzurePublic, TENANT_ID)
+                .WithRedirectUri(REDIRECT_URI)
+                .Build();           
+        
+            string accessToken = string.Empty;
+            Console.WriteLine("im here");
+            AuthenticationResult authResult = null;
+            IEnumerable<IAccount> accounts = await app.GetAccountsAsync();
+            
+            Console.WriteLine("im here 2");            
+            try
+            {
+                            Console.WriteLine("im here 3");
+                authResult = await app.AcquireTokenSilent(scopes, accounts.FirstOrDefault()).ExecuteAsync();
+                            Console.WriteLine("im here 4");
+                accessToken = authResult.AccessToken;
+            } catch (MsalUiRequiredException)
+            {
+                            Console.WriteLine("im here 5");
+                authResult = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+                             Console.WriteLine("im here 6");
+                accessToken = authResult.AccessToken;
+            } catch (Exception ex)
+            {
+                Console.WriteLine($"Authentication error: {ex.Message}");
+            }
+        
+            Console.WriteLine($"Access token: {accessToken}\n");
+
+            return accessToken;
+        }
+
+        public static string GetToken()
+        {
+            return GetAccessToken_UserInteractive().Result;
+        }
+
         public static async Task<string> GetAccessToken_ClientCredentials()
         {
         
