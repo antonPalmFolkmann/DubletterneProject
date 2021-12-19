489f727837e0548090743a4d45d25ca0a9e91b65
aHjelmgaard <90194013+aHjelmgaard@users.noreply.github.com>
2021-12-19 21:18:30 +0100

    Feature/search implementation (#59)
    * Started implementing search functionality.
    
    * Updates to 'SearchController' and 'Resources'
    
    * Implemented search-as-you-type for resources
    
    * Succesfully implemented search functionality!!
    
    Co-authored-by: Alexander Hjelmgaard <hjel@itu.dk>
    Co-authored-by: Nicklas Crawley Jørgensen <turuko44@gmail.com>

--- DubletterneAPP/Infrastructure/search/MatchSorter.cs
+++ DubletterneAPP/Infrastructure/search/MatchSorter.cs
@@ -2,11 +2,23 @@ namespace Infrastructure.Search{
 
     public class MatchSorter{
     
-        public static List<ISearchAble> SortMatchesKeysHighestScoreFirst(Dictionary<ISearchAble, int> matches){
+        public static List<UserDTO> SortMatchesKeysHighestScoreFirst(Dictionary<UserDTO, int> matches){
             var keyValueList = matches.ToList();
             keyValueList.Sort((x, y) => y.Value.CompareTo(x.Value));
 
-            var list = new List<ISearchAble>();
+            var list = new List<UserDTO>();
+            for (int index = 0; index < keyValueList.Count; index++){
+                list.Add(keyValueList[index].Key);
+            }
+            
+            return list;
+        }
+
+        public static List<ResourceDTO> SortMatchesKeysHighestScoreFirst(Dictionary<ResourceDTO, int> matches){
+            var keyValueList = matches.ToList();
+            keyValueList.Sort((x, y) => y.Value.CompareTo(x.Value));
+
+            var list = new List<ResourceDTO>();
             for (int index = 0; index < keyValueList.Count; index++){
                 list.Add(keyValueList[index].Key);
             }
--- DubletterneAPP/Infrastructure/search/SearchValidater.cs
+++ DubletterneAPP/Infrastructure/search/SearchValidater.cs
@@ -5,7 +5,7 @@ namespace Infrastructure.Search{
     public static class SearchValidater
     {
         //If no more methods is added. This thould be moved into the SearchRequestForm
-        private static Regex regex = new Regex(@"^[a-zA-ZæøåÆØÅ,\.\/\\\(\)\[\]\!\?\-\;\:\s]{0,127}$", RegexOptions.Compiled);
+        private static Regex regex = new Regex(@"^[0-9a-zA-ZæøåÆØÅ,\.\/\\\(\)\[\]\!\?\-\;\:\s]{0,127}$", RegexOptions.Compiled);
         
         public static bool ValidateSearchTermCharacters(string streng){
             return regex.IsMatch(streng);
